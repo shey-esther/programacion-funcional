adcf204617c285cfd45a0cfd858d7848dc2c1939
esther <estheralvaro44@gmail.com>
2017-09-20 09:58:39 -0500

    programacion funcional

---
+++ assets/js/main.js
@@ -0,0 +1,56 @@
+// aqui el codigo js
+function intersection (l1,l2) {
+  var l3 = [];
+  for (var i = 0; i < l1.length; i++) {
+    for (var j = 0; j < l2.length; j++) {
+      if (l1[i] == l2[j]) {
+        l3.push(l1[i]);
+      }      
+    }    
+  }
+  return l3;
+}
+
+function intersectionSet2(l1, l2) {
+  return l1.filter (function(n) {
+    return l2.indexOf (n) !== -1
+  });
+};
+
+var productos = [
+  {
+    nombre : 'pijama',
+    tipo : 'ropa'
+  },
+  {
+    nombre : 'nevera',
+    tipo : 'electrodomestico'
+  },
+  {
+    nombre : 'asus',
+    tipo : 'computador'
+  },
+  {
+    nombre : 'macbook',
+    tipo : 'computador'
+  },
+  {
+    nombre : 'pijama',
+    tipo : 'ropa'
+  },
+  {
+    nombre : 'toshiba',tipo : 'computador'
+  }
+];
+
+var computadores = [];
+for (var i = 0; i < productos.length; i++) {
+  if (productos[i].tipo === 'computador') {
+    computadores.push(productos[i]);
+  }
+}
+
+var computadores = productos.filter(function (producto) {
+  return productos.tipo === 'computador'
+});
+
